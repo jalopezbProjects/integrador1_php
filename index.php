2f5cbc9014e2c9d9600937d695b7188169e949dc
Andrés L <23505680+andresbelo12@users.noreply.github.com>
2020-08-13 18:12:15 -0500

    CSS + HTML

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <meta charset="UTF-8">
+        <title>Juan Andres Tutorial</title>
+    </head>
+    <body>
+        <nav id="navigation_bar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="game.php">Let's Play</a></li>
+              </ul>
+        </nav>
+        <header>
+            <div class="container"><h1 class = "welcome_msg">Welcome to Juan Andres Lopez Page</h1></div>
+        </header>
+        <div class = "about">
+            <p class="about_msg">Hi, welcome to my first web application using HTML + CSS + PHP</p>
+            <h1 class = "about_title">Who I Am?</h1>
+            <p class = "about_info">
+                I'm a colombian 19 years old man, I'm currently stuying System Engineering at EAFIT University - Medellin.
+                I started programming when I was 15 years old at my high school INEM José Félix de Restrepo, where I learnt some
+                OOP Programming, Java and HTML. I love to play video games with my friends like Fortnite, GTA V, and others. One of my
+                hobbies is to sing, I'm a Tenor singer in EAFIT Choir, and I also love music, I love almost al genres and types of 
+                music as Rock, Metal, Reggaeton, Trap, Old Latin music, Hip Hop, Classical Music and Opera.
+            </p>
+        </div>
+        <div class = "experience">
+            <h1 class="experience_title">My Experience</h1>           
+            <table class="experience_table">
+                <tr>
+                    <th>
+                        <p>Desktop & Mobile</p>
+                        <img src="images/java_main.png">
+                    </th>
+                    <th>
+                        <p>Web</p>
+                        <img src="images/aws.png">
+                    </th>
+                    <th>
+                        <p>Mobile</p>
+                        <img src="images/flutter-logo-sharing.png">
+                    </th>
+                    <th>
+                        <p>Data Structure and Algorithms</p>
+                        <img src="images/python.png">
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Created an emergency system where regular people using their cellphone can send
+                            messages, images, alerts and others to the emergency systems like red cross which had
+                            a different desktop app to receive alerts
+                        </p>
+                    </td>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                    <td>
+                        <p>Created a web application using stack MERN, deploying code in AWS using load balancers and a
+                            custom DNS service and with a custom domain in GoDaddy</p>
+                    </td>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                    <td>
+                        <pre>     ...</pre>
+                    </td>
+                </tr>
+            </table>           
+        </div>
+        <footer class="footer">
+            <div>
+                <p>Contact: andresjlopez12@gmail.com</p>
+            </div>
+        </footer>   
+    </body>
+</html>
